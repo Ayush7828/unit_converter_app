3d8f73cc9c7f04c07214ac233c3832daf2e45d68
Ayush Singh <singhayush886@gmail.com>
2022-07-25 19:09:09 +0530

    Unit_App

--- lib/pages/spalsh_screen.dart
+++ lib/pages/spalsh_screen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigator() async {
-    await Future.delayed(const Duration(seconds: 4), () {});
+    await Future.delayed(const Duration(seconds: 20), () {});
     // ignore: use_build_context_synchronously
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const UnitScreen()));
@@ -26,22 +26,39 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
-                "assets/images/hiiii.png",
-                height: 120,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xffEEAECA),
+                Color(0xff94BBE9),
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  "assets/images/hiiii.png",
+                  height: 120,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const CircularProgressIndicator(
-              color: Color.fromARGB(255, 167, 113, 109),
-            )
-          ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text(
+                "Unit Conveter App",
+                style: TextStyle(
+                    fontFamily: "Poppins",
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              const CircularProgressIndicator(
+                color: Color.fromARGB(255, 167, 113, 109),
+              )
+            ],
+          ),
         ),
       ),
     );
